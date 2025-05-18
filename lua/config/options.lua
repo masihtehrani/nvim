e3d80aefe72eb42c13ceb2683d862302f9349b89
F3U23 <mahmud.tehrani@gmail.com>
2025-05-18 16:03:08 +0330

    persian

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.termbidi = true
+vim.opt.arabicshape = true
+-- vim.o.guifont = 'Vazir:h12'
