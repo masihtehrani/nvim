7b068d7cf18534bc27a70ffbf442e247ee919f1a
F3U23 <mahmud.tehrani@gmail.com>
2025-05-28 13:20:35 +0330

    add golang

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -311,6 +311,70 @@ return {
       },
     },
   },
+  {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      gopls = {
+        settings = {
+          gopls = {
+            gofumpt = true,
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            analyses = {
+              nilness = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            semanticTokens = true,
+          },
+        },
+      },
+    },
+    setup = {
+      gopls = function(_, opts)
+        -- workaround for gopls not supporting semanticTokensProvider
+        -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+        LazyVim.lsp.on_attach(function(client, _)
+          if not client.server_capabilities.semanticTokensProvider then
+            local semantic = client.config.capabilities.textDocument.semanticTokens
+            client.server_capabilities.semanticTokensProvider = {
+              full = true,
+              legend = {
+                tokenTypes = semantic.tokenTypes,
+                tokenModifiers = semantic.tokenModifiers,
+              },
+              range = true,
+            }
+          end
+        end, "gopls")
+        -- end workaround
+      end,
+    },
+  },
+  },
 
   -- add more treesitter parsers
   {
@@ -333,6 +397,10 @@ return {
         "yaml",
         "rust",
         "ron",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
       },
     },
   },
@@ -376,26 +444,7 @@ return {
   },
 
   -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-        "goimports",
-        "gofumpt",
-        "gomodifytags",
-        "impl",
-        "delve",
-      },
-      formatters_by_ft = {
-        go = { "goimports", "gofumpt" },
-      },
-    },
-  },
-
+  
   -- {
   --   "milanglacier/minuet-ai.nvim",
   --   event = "VeryLazy",
@@ -702,22 +751,69 @@ return {
   end,
   },
   {
-  "mason-org/mason.nvim",
-  optional = true,
-  opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, { "codelldb" })
-    if diagnostics == "bacon-ls" then
-      vim.list_extend(opts.ensure_installed, { "bacon" })
-    end
-  end,
-  },
-  {
   "nvim-neotest/neotest",
   optional = true,
+  dependencies = {
+    "fredrikaverpil/neotest-golang",
+  },
   opts = {
     adapters = {
       ["rustaceanvim.neotest"] = {},
+      ["neotest-golang"] = {
+        -- Here we can set options for neotest-golang, e.g.
+        -- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+        dap_go_enabled = true, -- requires leoluz/nvim-dap-go
+      },
+    },
+  },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "impl",
+        "delve",
+        "codelldb",
+        "bacon",
+      },
+      formatters_by_ft = {
+        go = { "goimports", "gofumpt" },
+      },
+    },
+  },
+  {
+  "leoluz/nvim-dap-go",
+  opts = {},
+  },
+  {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
+    },
+    filetype = {
+      gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+    },
+  },
+  },
+  {
+  "mfussenegger/nvim-dap",
+  optional = true,
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = { ensure_installed = { "delve" } },
+    },
+    {
+      "leoluz/nvim-dap-go",
+      opts = {},
     },
   },
   },
